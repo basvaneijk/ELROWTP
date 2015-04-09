c0f879e474d04a8596959823b93f2c0d0424caf0
Olaf van der Kruk <olafvdkruk@gmail.com>
2015-04-09 16:43:06 +0200

    Blobtracking added

--- Red Object Detection/test1/test1.cpp
+++ Red Object Detection/test1/test1.cpp
@@ -1,19 +1,106 @@
 #include "stdafx.h"
+
 #include <iostream>
-#include "opencv2/highgui/highgui.hpp"
-#include "opencv2/imgproc/imgproc.hpp"
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/opencv.hpp>
 
 using namespace cv;
 using namespace std;
 
+Mat filterUsingHSV(Mat frame,int iLowH, int iHighH, int iLowS, int iHighS, int iLowV, int iHighV){
+	Mat imgHSV;
+
+	cvtColor(frame, imgHSV, COLOR_BGR2HSV); //Convert the captured frame from BGR to HSV
+
+	Mat imgThresholded;
+
+	inRange(imgHSV, Scalar(iLowH, iLowS, iLowV), Scalar(iHighH, iHighS, iHighV), imgThresholded); //Threshold the image
+
+	//morphological opening (removes small objects from the foreground)
+	erode(imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)));
+	dilate(imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)));
+
+	//morphological closing (removes small holes from the foreground)
+	dilate(imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)));
+	erode(imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)));
+
+	
+	return imgThresholded;
+}
+
+Mat drawRedFollowLine(Mat frame) {
+	int iLastX = -1;
+	int iLastY = -1;
+	//Calculate the moments of the thresholded image
+	Moments oMoments = moments(frame);
+	Mat imgLines = Mat::zeros(frame.size(), CV_8UC3);;
+
+	double dM01 = oMoments.m01;
+	double dM10 = oMoments.m10;
+	double dArea = oMoments.m00;
+
+	// if the area <= 10000, I consider that the there are no object in the image and it's because of the noise, the area is not zero 
+	if (dArea > 10000)
+	{
+		//calculate the position of the ball
+		int posX = dM10 / dArea;
+		int posY = dM01 / dArea;
+
+		if (iLastX >= 0 && iLastY >= 0 && posX >= 0 && posY >= 0)
+		{
+			//Draw a red line from the previous point to the current point
+			line(imgLines, Point(posX, posY), Point(iLastX, iLastY), Scalar(0, 0, 255), 2);
+		}
+
+		iLastX = posX;
+		iLastY = posY;
+	}
+	return imgLines;
+}
+Mat blobTrack(Mat im){
+
+	// Setup SimpleBlobDetector parameters.
+	SimpleBlobDetector::Params params;
+	// Change thresholds
+	params.minThreshold = 10;
+	params.maxThreshold = 200;
+	// Filter by Area.
+	params.filterByArea = false;
+	params.minArea = 15;
+	// Filter by Circularity
+	params.filterByCircularity = true;
+	params.minCircularity = 0.01;
+	// Filter by Convexity
+	params.filterByConvexity = true;
+	params.minConvexity = 0.03;
+	// Filter by Inertia
+	params.filterByInertia = false;
+	params.minInertiaRatio = 0.01;
+	// Storage for blobs
+	vector<KeyPoint> keypoints;
+#if CV_MAJOR_VERSION < 3 // If you are using OpenCV 2
+	// Set up detector with params
+	SimpleBlobDetector detector(params);
+	// Detect blobs
+	detector.detect(im, keypoints);
+#else
+	// Set up detector with params
+	Ptr<SimpleBlobDetector> detector = SimpleBlobDetector::create(params);
+	// Detect blobs
+	detector->detect(im, keypoints);
+#endif
+	// Draw detected blobs as red circles.
+	// DrawMatchesFlags::DRAW_RICH_KEYPOINTS flag ensures
+	// the size of the circle corresponds to the size of blob
+	Mat im_with_keypoints;
+	drawKeypoints(im, keypoints, im_with_keypoints, Scalar(0, 0, 255), DrawMatchesFlags::DRAW_RICH_KEYPOINTS);
+	return im_with_keypoints;
+}
 int main(int argc, char** argv)
 {
-	VideoCapture cap(0); //capture the video from webcam
-	cap.set(6, 1); //FPS
-	cap.set(CV_CAP_PROP_FRAME_WIDTH, 1280); //Width
-	cap.set(CV_CAP_PROP_FRAME_HEIGHT, 720); //Height
-	cap.set(16, 0.1); //exposure
-
+	VideoCapture cap(1); //capture the video from webcam
+	cap.set(CV_CAP_PROP_SETTINGS, 1);
 	if (!cap.isOpened())  // if not success, exit program
 	{
 		cout << "Cannot open the web cam" << endl;
@@ -22,37 +109,29 @@ int main(int argc, char** argv)
 
 	namedWindow("Control", CV_WINDOW_AUTOSIZE); //create a window called "Control"
 
-	//int iLowH = 61;//60;
-	//int iHighH = 179;//50;
-	int iLowH = 36;
-	int iHighH = 49;
+	//RED
+	int iLowH_red = 170;
+	int iHighH_red = 179;
 
-	int iLowS = 0;
-	int iHighS = 13;//15;
+	int iLowS_red = 150;
+	int iHighS_red = 255;
 
-	int iLowV = 250;
+	int iLowV_red = 60;
+	int iHighV_red = 255;
+
+	//BLUE
+	int iLowH = 102;
+	int iHighH = 120;
+
+	int iLowS = 168;
+	int iHighS = 255;
+
+	int iLowV = 103;
 	int iHighV = 255;
-
-	//Create trackbars in "Control" window
-	createTrackbar("LowH", "Control", &iLowH, 179); //Hue (0 - 179)
-	createTrackbar("HighH", "Control", &iHighH, 179);
-
-	createTrackbar("LowS", "Control", &iLowS, 255); //Saturation (0 - 255)
-	createTrackbar("HighS", "Control", &iHighS, 255);
-
-	createTrackbar("LowV", "Control", &iLowV, 255);//Value (0 - 255)
-	createTrackbar("HighV", "Control", &iHighV, 255);
-
-	int iLastX = -1;
-	int iLastY = -1;
 
 	//Capture a temporary image from the camera
 	Mat imgTmp;
 	cap.read(imgTmp);
-
-	//Create a black image with the size as the camera output
-	Mat imgLines = Mat::zeros(imgTmp.size(), CV_8UC3);;
-
 
 	while (true)
 	{
@@ -60,57 +139,21 @@ int main(int argc, char** argv)
 
 		bool bSuccess = cap.read(imgOriginal); // read a new frame from video
 
-
-
 		if (!bSuccess) //if not success, break loop
 		{
 			cout << "Cannot read a frame from video stream" << endl;
 			break;
 		}
 
-		Mat imgHSV;
+		Mat imgThresholded = filterUsingHSV(imgOriginal, iLowH, iHighH, iLowS, iHighS, iLowV, iHighV);
+		Mat imgThresholded2 = filterUsingHSV(imgOriginal, iLowH_red, iHighH_red, iLowS_red, iHighS_red, iLowV_red, iHighV_red);
+		
+		Mat imgResult = imgThresholded + imgThresholded2;
+		Mat result = blobTrack(imgResult);
 
-		cvtColor(imgOriginal, imgHSV, COLOR_BGR2HSV); //Convert the captured frame from BGR to HSV
-
-		Mat imgThresholded;
-
-		inRange(imgHSV, Scalar(iLowH, iLowS, iLowV), Scalar(iHighH, iHighS, iHighV), imgThresholded); //Threshold the image
-
-		//morphological opening (removes small objects from the foreground)
-		erode(imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)));
-		dilate(imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)));
-
-		//morphological closing (removes small holes from the foreground)
-		dilate(imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)));
-		erode(imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)));
-
-		//Calculate the moments of the thresholded image
-		Moments oMoments = moments(imgThresholded);
-
-		double dM01 = oMoments.m01;
-		double dM10 = oMoments.m10;
-		double dArea = oMoments.m00;
-
-		// if the area <= 10000, I consider that the there are no object in the image and it's because of the noise, the area is not zero 
-		if (dArea > 10000)
-		{
-			//calculate the position of the ball
-			int posX = dM10 / dArea;
-			int posY = dM01 / dArea;
-
-			if (iLastX >= 0 && iLastY >= 0 && posX >= 0 && posY >= 0)
-			{
-				//Draw a red line from the previous point to the current point
-				line(imgLines, Point(posX, posY), Point(iLastX, iLastY), Scalar(0, 0, 255), 2);
-			}
-
-			iLastX = posX;
-			iLastY = posY;
-		}
-
-		imshow("Thresholded Image", imgThresholded); //show the thresholded image
-
-		imgOriginal = imgOriginal + imgLines;
+		imshow("Thresholded Image", result); //show the thresholded image
+		//
+		//imgOriginal += drawRedFollowLine(imgResult);
 		imshow("Original", imgOriginal); //show the original image
 
 		if (waitKey(30) == 27) //wait for 'esc' key press for 30ms. If 'esc' key is pressed, break loop
@@ -122,3 +165,4 @@ int main(int argc, char** argv)
 
 	return 0;
 }
+
