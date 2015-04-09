15e3147fb8f532c8226524a21f47a4ab14c92d51
Niek Arends <niek.arends@student.hu.nl>
2015-04-09 15:18:37 +0200

    Added tracking objects via hsv color

---
+++ Movement Detection/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,35 @@
+#include<opencv2/opencv.hpp>
+#include<opencv2/video/background_segm.hpp>
+#include<iostream>
+#include<vector>
+
+int main(int argc, char *argv[])
+{
+	cv::Mat frame;
+	cv::Mat back;
+	cv::Mat fore;
+	cv::VideoCapture cap(1);
+	cv::BackgroundSubtractorMOG2 bg(30,4,false);
+	//bg.nmixtures = 3;
+	//bg.bShadowDetection = false;
+
+	std::vector<std::vector<cv::Point> > contours;
+
+	cv::namedWindow("Frame");
+	cv::namedWindow("Background");
+
+	for (;;)
+	{
+		cap >> frame;
+		bg.operator ()(frame, fore);
+		bg.getBackgroundImage(back);
+		cv::erode(fore, fore, cv::Mat());
+		cv::dilate(fore, fore, cv::Mat());
+		cv::findContours(fore, contours, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_NONE);
+		cv::drawContours(frame, contours, -1, cv::Scalar(0, 0, 255), 2);
+		cv::imshow("Frame", frame);
+		cv::imshow("Background", back);
+		if (cv::waitKey(30) >= 0) break;
+	}
+	return 0;
+}
