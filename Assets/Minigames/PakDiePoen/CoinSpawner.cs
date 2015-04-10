cddfa50b566381d6287bec7f923515a391d7898e
Simon Voordouw <simonvoordouw@gmail.com>
2015-04-10 13:48:27 +0200

    add coinspawner to scene

--- Assets/Minigames/PakDiePoen/CoinSpawner.cs
+++ Assets/Minigames/PakDiePoen/CoinSpawner.cs
@@ -7,28 +7,33 @@ public class CoinSpawner : MonoBehaviour
 {
 	public GameObject coinPrefab;
 	public int numCoins;
-
 	List<GameObject> coins;
-	// Use this for initialization
+
 	void Start ()
 	{
-		coins = new List<GameObject> (numCoins);
+		coins = new List<GameObject> ();
 
-		var mt = TrackerManager.Instance.InitTracker<MarkerTracker> ();
-		//MarkerTracker mt = TrackerManager.Instance.GetTracker<MarkerTracker> ();
 
-		for (int c = 0; c < numCoins; c++) {
-			var newCoin = Instantiate (coinPrefab);
-		
+	}
 
-			var marker = mt.CreateMarker (c, "coinMarker" + c, 60);
-			marker.gameObject.AddComponent<DefaultTrackableEventHandler> ();
-			coins.Add (marker.gameObject);
-			marker.transform.parent = this.transform;
-			newCoin.transform.parent = marker.transform;
+	void Update ()
+	{
+		if (QCARManager.Instance.Initialized) {
+
+			CameraDevice.Instance.Stop ();
+			//var mt = TrackerManager.Instance.InitTracker<MarkerTracker> ();
+			MarkerTracker mt = TrackerManager.Instance.GetTracker<MarkerTracker> ();
+
+			for (int c = 0; c < numCoins; c++) {
+				var marker = mt.CreateMarker (c, "coinMarker" + c, 60);
+				marker.gameObject.AddComponent<DefaultTrackableEventHandler> ();
+				coins.Add (marker.gameObject);
+				marker.transform.parent = this.transform;
+				
+				var newCoin = Instantiate (coinPrefab);
+				newCoin.transform.parent = marker.transform;
+			}
+			CameraDevice.Instance.Start ();
 		}
-
-
-
 	}
 }
