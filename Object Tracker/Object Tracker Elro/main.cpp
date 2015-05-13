69ba42cdafd3ca5db99b48d5adbff0ff5530f50c
Niek Arends <niek.arends@student.hu.nl>
2015-05-13 15:42:13 +0200

    Fixed some issues
    Compiling etc

---
+++ Object Tracker/Object Tracker Elro/main.cpp
@@ -0,0 +1,6 @@
+#include "tracker.h"
+
+int main() {
+	tracker t(0);
+	t.trackObjects();
+}
--- Object Tracker/Object Tracker Elro/tracker.h
+++ Object Tracker/Object Tracker Elro/tracker.h
@@ -5,34 +5,30 @@
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/opencv.hpp>
 
+using namespace cv;
+using namespace std;
+
 struct KeyPointColor{
 	KeyPoint keypoint;
 	Vec3b color;
 };
 
-using namespace cv;
-using namespace std;
 
 class tracker
 {
 public:
 	tracker(VideoCapture cap);
 	~tracker();
-
+	void trackObjects();
 private:
 	VideoCapture cap;
-	int iLowH, iHighH, iLowS, iHighS, iLowV, iHighV;
+	int iLowH = 0, iHighH = 179, iLowS = 85, iHighS = 255, iLowV = 108, iHighV = 255;
 	vector<KeyPointColor> trackingResult;
 
 	Mat filterUsingHSV(Mat&, int iLowH, int iHighH, int iLowS, int iHighS, int iLowV, int iHighV);
-	vector<KeyPoint> trackBlob(Mat &);	
+	vector<KeyPoint> trackBlob(Mat &);
 	vector<KeyPointColor> getKeypointColors(Mat&, vector<KeyPoint>&);
 	Mat drawPoints(Mat img, vector<KeyPointColor> keypointcolors);
 
-	void trackObjects();
-
-
-
 	
 };
-
