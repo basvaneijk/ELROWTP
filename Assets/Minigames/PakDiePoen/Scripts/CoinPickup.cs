0aca866a75b0616b4a8fa3f70876cbe439979320
peter-markotic <peter.markotic@student.hu.nl>
2015-05-20 15:17:11 +0200

    created scripts folder

---
+++ Assets/Minigames/PakDiePoen/Scripts/CoinPickup.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CoinPickup : MonoBehaviour
+{
+	void OnTriggerEnter (Collider other)
+	{
+		if (other.name == "Reticle") {
+			gameObject.SetActive (false);
+		}
+	}
+}
---
+++ Assets/Minigames/PakDiePoen/Scripts/Timer.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Timer : MonoBehaviour {
+	public float minutes = 0f;
+	public float seconds = 0f;
+	public float milliseconds = 0f;
+
+
+	// Use this for initialization
+	void Start () {
+		//seconds = 100f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		seconds += Time.deltaTime;
+		milliseconds += Time.deltaTime;
+	}
+}
