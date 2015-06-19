99afe9f5e959f6a088709f2830a132ed4c035673
Jelmer Bootsma <contact@jelmerbootsma.nl>
2015-06-19 14:44:15 +0200

    Implement MG2 in Main project

---
+++ Games/Assets/Minigames/EtenBezorgen/Scripts/kitchenScript.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Timers;
+using System.Collections.Generic;
+
+public class kitchenScript : MonoBehaviour {
+	private int food;
+	private bool foodReady = false;
+	private float foodInterval = 5;
+	private float timer = 0;
+	public GameObject[] foodModels;
+	private int foodCount = 0;
+	private int presentedFood = 0;
+	private GameObject foodOnTableModel;
+	// Use this for initialization
+	void Start () {
+		timer = foodInterval;
+		foodCount = foodModels.Length;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timer -= Time.deltaTime;
+		if (timer < 0) {
+			// food ready
+			prepareFood();
+		} else {
+			
+		}
+	}
+	
+	void prepareFood(){
+		if (!foodReady) {
+			print("Food Ready for pickup!");
+			foodReady = true;
+			timer = foodInterval;
+			presentFood();
+		}
+	}
+	void presentFood(){
+		presentedFood = Random.Range (0,foodCount -1);
+		GameObject tmp = Instantiate (foodModels [presentedFood], foodOnTableModel.transform.position, foodOnTableModel.transform.rotation) as GameObject;
+		Destroy (foodOnTableModel);
+		foodOnTableModel = tmp;
+	}
+	void OnCollisionEnter (Collision col)
+	{
+		if(col.gameObject.name == "waiter")
+		{
+			if(foodReady){
+				GameObject a = col.gameObject;
+				//a.GetComponent<waiter_plate>().giveFood(presentedFood); //cookingRequests.Dequeue()
+				foodReady = false;
+				timer = foodInterval;
+			}
+			
+			
+		}
+	}
+}
---
+++ Games/Assets/Minigames/EtenBezorgen/Scripts/trayScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class trayScript : MonoBehaviour {
+	public GameObject trayModel;
+	public GameObject[] foodModels;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// debug \\ int a = Random.Range (0, foodModels.Length-1);
+
+	}
+	public void setFood(int a){
+		GameObject tmp = Instantiate (foodModels [a], trayModel.transform.position, trayModel.transform.rotation) as GameObject;
+		Destroy (trayModel);
+		trayModel = tmp;
+	}
+}
