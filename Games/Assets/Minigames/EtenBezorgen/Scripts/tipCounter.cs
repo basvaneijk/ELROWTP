e3c50bdb311e0e15c4c4dab15c0f0dd4076642c1
Simon Voordouw <simonvoordouw@gmail.com>
2015-06-22 13:49:40 +0200

    float can't be null

--- Games/Assets/Minigames/EtenBezorgen/Scripts/tipCounter.cs
+++ Games/Assets/Minigames/EtenBezorgen/Scripts/tipCounter.cs
@@ -2,62 +2,61 @@
 using System.Collections;
 using System;
 
-public class tipCounter : MonoBehaviour {
+public class tipCounter : MonoBehaviour
+{
 
-    private float score;
-    private float tip;
-    private DateTime time;
-    private Boolean isStarted = false;
-    private DateTime tempTime;
-    private int pause = 30;
+	private float score;
+	private float tip;
+	private DateTime time;
+	private Boolean isStarted = false;
+	private DateTime tempTime;
+	private int pause = 30;
 
 	// Use this for initialization
-	void Start () {
+	void Start ()
+	{
 	}
 	
 	// Update is called once per frame
-	void Update () {
-        if (isStarted)
-        {
-            tempTime = DateTime.Now;
-            if (tip > 0f && (tempTime.Hour > time.Hour || tempTime.Minute > time.Minute || tempTime.Second > time.Second))
-            {
-                if(pause > 0){
-                    pause--;
-                }else{
-                    tip -= 0.20f;
-                    time = tempTime;
-                }
-            }
-        }
+	void Update ()
+	{
+		if (isStarted) {
+			tempTime = DateTime.Now;
+			if (tip > 0f && (tempTime.Hour > time.Hour || tempTime.Minute > time.Minute || tempTime.Second > time.Second)) {
+				if (pause > 0) {
+					pause--;
+				} else {
+					tip -= 0.20f;
+					time = tempTime;
+				}
+			}
+		}
 	}
 
-    public void startGame()
-    {
-        time = DateTime.Now;
-        isStarted = true;
-        if (score == null)
-        {
-            score = 15f;
-        }
-        if (tip == null)
-        {
-            tip = 5f;
-        }
-    }
+	public void startGame ()
+	{
+		time = DateTime.Now;
+		isStarted = true;
+		if (score == 0.0f) {
+			score = 15f;
+		}
+		if (tip == 0.0f) {
+			tip = 5f;
+		}
+	}
 
-    public float getScore()
-    {
-        return score + tip;
-    }
+	public float getScore ()
+	{
+		return score + tip;
+	}
 
-    public void setScore(float points)
-    {
-        this.score = points;
-    }
+	public void setScore (float points)
+	{
+		this.score = points;
+	}
 
-    public void setTip(float tipmoney)
-    {
-        this.tip = tipmoney;
-    }
+	public void setTip (float tipmoney)
+	{
+		this.tip = tipmoney;
+	}
 }
