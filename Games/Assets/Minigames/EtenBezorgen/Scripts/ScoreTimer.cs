a18e560368de71d13b54d2e3f4b73b3a82a5462b
HoekmanJW <jhoekman@live.nl>
2015-06-22 12:40:55 +0200

    New version

--- Games/Assets/Minigames/EtenBezorgen/Scripts/ScoreTimer.cs
+++ Games/Assets/Minigames/EtenBezorgen/Scripts/ScoreTimer.cs
@@ -4,27 +4,54 @@ using System;
 
 public class ScoreTimer : MonoBehaviour {
 
-    private float score = 15f;
-    private float tip = 5f;
+    private float score;
+    private float tip;
     private DateTime time;
+    private Boolean isStarted = false;
 
 	// Use this for initialization
 	void Start () {
-        time = DateTime.Now;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        DateTime tempTime = DateTime.Now;
-        if (tip > 0f && (tempTime.Hour > time.Hour || tempTime.Minute > time.Minute || tempTime.Second > time.Second) )
+        if (isStarted)
         {
-            tip -= 0.20f;
-            time = tempTime;
+            DateTime tempTime = DateTime.Now;
+            if (tip > 0f && (tempTime.Hour > time.Hour || tempTime.Minute > time.Minute || tempTime.Second > time.Second))
+            {
+                tip -= 0.20f;
+                time = tempTime;
+            }
         }
 	}
+
+    public void startGame()
+    {
+        time = DateTime.Now;
+        isStarted = true;
+        if (score == null)
+        {
+            score = 15f;
+        }
+        if (tip == null)
+        {
+            tip = 5f;
+        }
+    }
 
     public float getScore()
     {
         return score + tip;
+    }
+
+    public void setScore(float points)
+    {
+        this.score = points;
+    }
+
+    public void setTip(float tipmoney)
+    {
+        this.tip = tipmoney;
     }
 }
