a576cd67e9a01fcfe8bd7b66dfc5b173cdf58aff
Simon Voordouw <simonvoordouw@gmail.com>
2015-06-25 11:39:36 +0200

    License server under GPL

--- PositionServer/src/tracker.cpp
+++ PositionServer/src/tracker.cpp
@@ -1,3 +1,23 @@
+/*
+    Positiontracker & server for ELRO Wants To Play
+    Copyright (C) 2015 Niek Arends
+    Copyright (C) 2015 Olaf van der Kruk
+    Copyright (C) 2015 Simon Voordouw
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include <exception>
 #include <cstdio>
 #include <vector>
